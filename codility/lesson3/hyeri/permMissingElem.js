3d44e20ad966a0dd1823c6034269c3f44f513f34
Jetom88 <shinhro3o@gmail.com>
2022-06-19 23:18:25 +0900

    feat: permMissingElem.js 코드 추가 및 .md 내용 추가

--- codility/lesson3/hyeri/permMissingElem.js
+++ codility/lesson3/hyeri/permMissingElem.js
@@ -11,3 +11,22 @@ function solution(A) {
     }
   }
 }
+
+//두 번째 코드(100%)
+
+function solution(A) {
+  if (A.length === 0) {
+    return 1;
+  }
+
+  A.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; i < A.length; i++) {
+    if (i + 1 !== A[i]) {
+      return i + 1;
+    }
+  }
+  return A.length + 1;
+}
