b98f979494da94548efdb36e4784e80515c9520c
Jetom88 <shinhro3o@gmail.com>
2022-06-25 21:04:32 +0900

    docs: js 파일이 안올라가서 재 push..

--- codility/lesson3/hyeri/frogRiverOne.js
+++ codility/lesson3/hyeri/frogRiverOne.js
@@ -9,3 +9,17 @@ function solution(X, A) {
     }
   }
 }
+
+//정답 코드
+function solution(X, A) {
+  const leaves = new Set();
+
+  for (let i = 0; i < A.length; i++) {
+    const now = A[i];
+    leaves.add(now);
+
+    if (leaves.size === X) return i;
+  }
+
+  return -1;
+}
