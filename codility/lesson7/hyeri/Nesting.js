a5ae66b8e5f08007c0f074cd1d975266333681ec
Jetom88 <shinhro3o@gmail.com>
2022-07-20 13:50:46 +0900

    docs: md 빠진 부분 추가 및 js 코드 수정

--- codility/lesson7/hyeri/Nesting.js
+++ codility/lesson7/hyeri/Nesting.js
@@ -2,12 +2,11 @@ function solution(S) {
   let arr = [];
 
   for (let i = 0; i < S.length; i++) {
-    let bracket = S[i];
-    if (bracket === "(") {
-      arr.push(bracket);
+    if (S[i] === "(") {
+      arr.push(S[i]);
     } else {
       if (arr.length === 0) return 0;
-      if (bracket === ")" && arr.pop() !== "(") return 0;
+      if (S[i] === ")" && arr.pop() !== "(") return 0;
     }
   }
   if (arr.length !== 0) return 0;
